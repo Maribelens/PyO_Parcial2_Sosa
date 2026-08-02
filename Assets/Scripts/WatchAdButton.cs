2531de36b83e131f613cf6e3fcf48d45ca60c6b6
Maribelens <maribelensosa@gmail.com>
2026-08-02 01:46:24 -0300

    feat(ui): add WatchAdButton script for handling rewarded ad requests and button state

---
+++ Assets/Scripts/WatchAdButton.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using RPGCombat.Ads;
+using System;
+
+namespace RPGCombat.UI
+{
+    // SRP Escucha el click y publica el evento
+    // No sabe nada de ads ni de recompensas
+    public class WatchAdButton : MonoBehaviour
+    {
+        [SerializeField] private Button button;
+        [SerializeField] private AdEventChannelSo adEventChannel;
+
+        private void Awake()
+        {
+            button.onClick.AddListener(OnButtonClicked);
+        }
+
+        private void OnEnable()
+        {
+            adEventChannel.OnRewardExpired += ResetButton;
+        }
+
+        private void OnDisable()
+        {
+            adEventChannel.OnRewardExpired -= ResetButton;
+        }
+
+        private void OnDestroy()
+        {
+            button.onClick.RemoveListener(OnButtonClicked);
+        }
+
+        private void OnButtonClicked()
+        {
+            button.interactable = false;    //evita doble click
+            adEventChannel.RaiseWatchAdRequested();
+        }
+
+        public void ResetButton() 
+        {
+            button.interactable = true;
+        }
+    }
+}

fb1a0a0f51a2ee33f051b247f9bdf132c1530706
Maribelens <maribelensosa@gmail.com>
2026-08-02 13:21:26 -0300

    add conditional logger for production build optimization
    - Se crea la clase utilitaria Log.cs usando System.Diagnostics.Conditional.
    - Se reemplazan las directivas #if UNITY_EDITOR por Log.Info en CombatActions.
    - Permite mantener logs en el Editor y Development Builds sin penalizar el rendimiento ni la memoria en la APK final de producción.

---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+namespace RPGCombat.Utils
+{
+    public static class Log
+    {
+        // Se incluirá en el Editor y en Builds de Desarrollo de Android/iOS
+        [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
+        public static void Info(string message)
+        {
+            UnityEngine.Debug.Log(message);
+        }
+
+        //logs que SIEMPRE aparezcan incluso en producción (como errores críticos)
+        public static void Error(string message)
+        {
+            UnityEngine.Debug.LogError(message);
+        }
+    }
+}
